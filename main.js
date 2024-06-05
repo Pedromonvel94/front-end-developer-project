ce97565c680a8e5c1169236b3a865dcb97ff8dc9
Pedromonvel94 <el_petercrauch2@hotmail.com>
2024-06-05 16:53:56 -0500

    adicion de funcionalidad del menu mobile y modificacion de css

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuHamIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+menuHamIcon.addEventListener("click", toggleMobileMenu);
 /*
 por si deseo que se llame a la funcion, en lugar de cuando ocurra el evento click, se haga cuando el mouse pase por encima de los componentes
 menuEmail.addEventListener("mouseenter", toggleDesktopMenu);
@@ -9,5 +13,10 @@ desktopMenu.addEventListener("mouseleave", toggleDesktopMenu);
 */
 
 function toggleDesktopMenu(){
-    desktopMenu.classList.toggle("inactive")
+    desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu(){
+    console.log("Di click");
+    mobileMenu.classList.toggle("inactive");
 }
